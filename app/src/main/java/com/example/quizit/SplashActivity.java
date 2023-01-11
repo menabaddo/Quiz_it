29c0ff7bf1c5f83015ea1d2f5847809ebecfed0a
menabaddo <edmundalexfresh@gmail.com>
2023-01-11 23:35:51 +0100

    Implemented Splashscreen with animation feature, pending color adjustments

--- app/src/main/java/com/example/quizit/SplashActivity.java
+++ app/src/main/java/com/example/quizit/SplashActivity.java
@@ -1,16 +1,43 @@
 package com.example.quizit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
-
+    private TextView appName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        appName = findViewById(R.id.app_name);
+
+        Typeface typeface = ResourcesCompat.getFont(this, R.font.skranji);
+        appName.setTypeface(typeface);
+
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.myanim);
+        appName.setAnimation(anim);
+
+        new Thread(){
+            @Override
+            public void run(){
+
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            }
+        }.start();
     }
 }
