0ae228eecb065815d63cd471c37c8705b3f31f4a
menabaddo <edmundalexfresh@gmail.com>
2023-01-13 17:45:41 +0100

    Done with the frontend working on the LoginActivity.java file.

--- app/src/main/java/com/example/quizit/LoginActivity.java
+++ app/src/main/java/com/example/quizit/LoginActivity.java
@@ -3,12 +3,26 @@ package com.example.quizit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email, pass;
+    private Button loginB;
+    private TextView forgotPassB, signupB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.password);
+        loginB = findViewById(R.id.loginButton);
+        forgotPassB = findViewById(R.id.forgot_pass);
+        signupB = findViewById(R.id.signupButton);
+
     }
 }
