a5b903fb028059806c29f45565874152f21a3ec0
menabaddo <edmundalexfresh@gmail.com>
2023-01-13 20:07:56 +0100

    Implemented back button for closing SignUpActivity and moving back to LoginActivity.

--- app/src/main/java/com/example/quizit/LoginActivity.java
+++ app/src/main/java/com/example/quizit/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.quizit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -32,6 +33,15 @@ public class LoginActivity extends AppCompatActivity {
                 if(validateData()){
                     login();
                 }
+            }
+        });
+
+
+        signupB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
             }
         });
 
--- app/src/main/java/com/example/quizit/SignUpActivity.java
+++ app/src/main/java/com/example/quizit/SignUpActivity.java
@@ -2,13 +2,45 @@ package com.example.quizit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText name, email, pass, confirmPass;
+    private Button signUpB;
+    private ImageView backB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        name = findViewById(R.id.username);
+        email = findViewById(R.id.emailID);
+        pass = findViewById(R.id.password);
+        confirmPass = findViewById(R.id.confirmPassword);
+        signUpB = findViewById(R.id.signUpB);
+        backB = findViewById(R.id.backButton);
+
+        backB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                finish();
+            }
+        });
+
+        signUpB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
