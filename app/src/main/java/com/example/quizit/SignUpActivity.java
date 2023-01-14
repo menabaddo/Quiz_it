b87e397c70efd4fcfe0570b4113885032fa7d8ae
menabaddo <edmundalexfresh@gmail.com>
2023-01-14 21:01:20 +0100

    Implemented Firebase authentication and added user validation for sign up in the app

--- app/src/main/java/com/example/quizit/SignUpActivity.java
+++ app/src/main/java/com/example/quizit/SignUpActivity.java
@@ -1,19 +1,30 @@
 package com.example.quizit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private EditText name, email, pass, confirmPass;
     private Button signUpB;
     private ImageView backB;
+    private FirebaseAuth mAuth;
+    private String emailStr, passStr, confirmPassStr, nameStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +38,20 @@ public class SignUpActivity extends AppCompatActivity {
         signUpB = findViewById(R.id.signUpB);
         backB = findViewById(R.id.backButton);
 
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+
+        signUpB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validate()) {
+                    signupNewUser();
+                }
+
+            }
+        });
+
         backB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,12 +60,70 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
 
-        signUpB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
 
     }
+
+
+    private void signupNewUser() {
+        mAuth.createUserWithEmailAndPassword(emailStr, passStr)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(SignUpActivity.this, "Successful Welcome To Quizit.",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            SignUpActivity.this.finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+
+    private Boolean validate() {
+
+        nameStr = name.getText().toString().trim();
+        passStr = pass.getText().toString().trim();
+        emailStr = email.getText().toString().trim();
+        confirmPassStr = confirmPass.getText().toString().trim();
+
+
+        if (nameStr.isEmpty()) {
+            name.setError("Enter Your Name");
+            return false;
+        }
+
+        if (emailStr.isEmpty()) {
+            email.setError("Enter Your Name");
+            return false;
+
+
+        }
+
+
+        if (passStr.isEmpty()) {
+            pass.setError("Enter Your Name");
+            return false;
+        }
+
+        if (confirmPassStr.isEmpty()) {
+            confirmPass.setError("Enter Your Name");
+            return false;
+        }
+
+        if (passStr.compareTo(confirmPassStr) != 0) {
+
+            Toast.makeText(SignUpActivity.this, "Password and Confirm Password Should be the same", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
 }
