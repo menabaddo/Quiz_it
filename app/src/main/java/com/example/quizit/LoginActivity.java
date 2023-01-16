eae3544768c98d849630444630275eb12e2fd46e
menabaddo <edmundalexfresh@gmail.com>
2023-01-16 16:32:48 +0100

    Implemented login functionality to my LoginActivity.java, so that existing user will be able to login.

--- app/src/main/java/com/example/quizit/LoginActivity.java
+++ app/src/main/java/com/example/quizit/LoginActivity.java
@@ -1,19 +1,29 @@
 package com.example.quizit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText email, pass;
     private Button loginB;
     private TextView forgotPassB, signupB;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +35,8 @@ public class LoginActivity extends AppCompatActivity {
         loginB = findViewById(R.id.loginButton);
         forgotPassB = findViewById(R.id.forgot_pass);
         signupB = findViewById(R.id.signupButton);
+
+        mAuth = FirebaseAuth.getInstance();
 
         loginB.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,6 +77,26 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private  void login(){
+
+        mAuth.signInWithEmailAndPassword(email.getText().toString().trim(), pass.getText().toString().trim())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                          Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+                          Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                          startActivity(intent);
+                          finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage(),
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
 
     }
 }
