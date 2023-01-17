5ace7cc6be86a71cd77b7aa8856d31a2c0247476
menabaddo <edmundalexfresh@gmail.com>
2023-01-17 16:29:11 +0100

    Workking on the test page for questions

---
+++ app/src/main/java/com/example/quizit/TestActivity.java
@@ -0,0 +1,14 @@
+package com.example.quizit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
